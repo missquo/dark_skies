926278d2c098d8bbdd67393132505dedcf0a770c
Rachel Price <rachel@south-austin.com>
2018-10-30 22:00:34 -0600

    Added second polygon to map

--- src/Map.js
+++ src/Map.js
@@ -50,6 +50,27 @@ export class MyMap extends Component {
 			{lat: 44.059578, lng: -114.5740057},
 			{lat: 44.200529, lng: -114.6564147}
 		];
+		
+		const darkSkyCore2 = [
+			{lat: 44.158658, lng: -114.9595197},
+			{lat: 44.1736498, lng: -115.018864},
+			{lat: 44.123008, lng: -115.0482977},
+			{lat: 44.096217, lng: -115.0006237},
+			{lat: 44.079259, lng: -115.0205047},
+			{lat: 44.035899, lng: -115.0492807},
+			{lat: 44.010657, lng: -114.9852037},
+			{lat: 43.991254, lng: -114.9890627},
+			{lat: 43.980898, lng: -114.9700257},
+			{lat: 43.971278, lng: -114.9736177},
+			{lat: 43.950408, lng: -114.9931647},
+			{lat: 43.937915, lng: -114.9740697},
+			{lat: 43.913675, lng: -114.9987737},
+			{lat: 43.864399, lng: -114.9796687},
+			{lat: 43.915051, lng: -114.8870567},
+			{lat: 44.080124, lng: -114.8953777}
+		];
+		
+		
 		const { listDisplay, google } = this.props
 
 		return (	
@@ -87,8 +108,18 @@ export class MyMap extends Component {
 					strokeOpacity={0.4}
 					strokeWeight={2}
 					fillColor="#4b0082"
-					fillOpacity={0.25}
+					fillOpacity={0.55}
 				/>
+				
+				<Polygon
+					paths={darkSkyCore2}
+					strokeColor="#4b0082"
+					strokeOpacity={0.4}
+					strokeWeight={2}
+					fillColor="#4b0082"
+					fillOpacity={0.55}
+				/>
+				
 			</Map>
 			</div>
 		);
