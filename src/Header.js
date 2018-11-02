5f2dad3f9d97432398ce1b880e86d4337bc8d495
Rachel Price <rachel@south-austin.com>
2018-11-01 21:22:25 -0600

    Finished list of map items; worked on responsive css

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 export class Header extends Component {
 	render() {
-
 		return (	
 			<nav>
-			<h1 tabIndex="0">Hidden Gems Around the US's First Dark Sky Reserve</h1>
+			<h1 tabIndex="0">Enjoying the US's First Dark Sky Reserve</h1>
 			<a href="#locationfilter" className="skip-link">Skip to main content - Filter locations by type</a>
 			</nav>
 		);
